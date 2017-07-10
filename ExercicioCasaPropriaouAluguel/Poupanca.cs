83dd9ac461b4879b3e2cb58e3904800b8159bbf1
camila <camilabarcellos.car@gmail.com>
2017-07-10 14:09:49 -0300

    CasaPropriaOuAluguel

---
+++ ExercicioCasaPropriaouAluguel/Poupanca.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioCasaPropriaouAluguel
+{
+    class Poupanca
+    {
+
+        public double SaldoDaPoupanca { get; set; }
+        public double Juros { get; set; }
+        public double ValorRendimento { get; set; }
+        public double ValorTotalMes { get; set; }
+        public double TotalContaAluguel { get; set; }
+        public int TempoMeses { get; set; }
+        public double ValorDoAluguel { get; set; }
+        public double ValorDeCompraDoImovel { get; set; }
+
+
+        
+
+        public void CalcularRendimentoDaPoupancaComSaqueDeAluguel()
+        {
+            ValorTotalMes = SaldoDaPoupanca;
+            for (var i = 1; i <= TempoMeses; i++) 
+            {
+                ValorRendimento = (((Juros / 100) * ValorTotalMes));
+                ValorTotalMes += ValorRendimento;
+                TotalContaAluguel = (((ValorTotalMes)) - ValorDoAluguel);
+            }
+            
+        }
+
+    }
+}
--- ExercicioCasaPropriaouAluguel/Program.cs
+++ ExercicioCasaPropriaouAluguel/Program.cs
@@ -10,6 +10,42 @@ namespace ExercicioCasaPropriaouAluguel
     {
         static void Main(string[] args)
         {
+            Poupanca poupanca = new Poupanca();
+
+            Console.WriteLine("Digite o valor investido na poupança:\n");
+            poupanca.SaldoDaPoupanca = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nDigite o valor de compra do imóvel:\n");
+            poupanca.ValorDeCompraDoImovel = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nDigite o valor do aluguel:\n");
+            poupanca.ValorDoAluguel = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nDigite o percentual de rendimento mensal da poupança:\n");
+            poupanca.Juros = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nDigite quantos meses terá o contrato de aluguel:\n");
+            poupanca.TempoMeses = Convert.ToInt32(Console.ReadLine());
+            
+
+            poupanca.CalcularRendimentoDaPoupancaComSaqueDeAluguel();
+
+            Console.WriteLine("\n-----------------------------------------------------------");
+
+                 if(poupanca.TotalContaAluguel > poupanca.SaldoDaPoupanca)
+                 {
+                     Console.WriteLine("\nÉ mais lucrativo alugar o imóvel.\nO rendimento da poupança será: R${0 :0.##} e o saldo final da conta será: {1}", poupanca.ValorRendimento, poupanca.TotalContaAluguel);
+                 }
+                 else if(poupanca.TotalContaAluguel < poupanca.SaldoDaPoupanca)
+                 {
+                     Console.WriteLine("\nÉ mais lucrativo comprar o imóvel.\nO rendimento da poupança não será suficiente.");
+                 }
+                 else
+                 {
+                     Console.WriteLine("\nO saldo final será o mesmo tanto para comprar quanto para alugar o imóvel nessas condições.");
+                 }
+                 
+            Console.ReadLine();
         }
     }
 }
